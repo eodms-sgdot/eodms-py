c3db7a38d7b86c05ac4e7c7d511beca27d2cc930
willmacs <william.mackinnon@nrcan-rncan.gc.ca>
2026-06-02 15:03:47 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='eodms-py',
-    version='2026.05.29', 
+    version='2026.06.02', 
     author='EODMS (Natural Resources Canada)',
     author_email='eodms-sgdot@nrcan-rncan.gc.ca',
     packages=find_packages(),
